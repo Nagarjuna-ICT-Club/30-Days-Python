56176920ac0dd1d1ab70bf32e1de42ac9a6d69b0
NickName-AM <maharjanebik061@gmail.com>
2024-02-17 19:43:41 +0545

    Day01

---
+++ Day01/day01.py
@@ -0,0 +1,10 @@
+# A python comment starts with a hash(#)
+
+
+# Displays the string 'Hello World'
+print('Hello World')
+print("Hello World")
+# Strings can be wrapped around single('') or double("") quotes
+
+# String concatenation
+print('Hello ' + 'World')
