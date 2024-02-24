993f3733d8816a53bca6f4bbf0707ec2165c2169
NickName-AM <maharjanebik061@gmail.com>
2024-02-24 21:37:32 +0545

    Day02

---
+++ Day02/day02.py
@@ -0,0 +1,23 @@
+# Primitive
+my_int = 4
+pi = 3.14
+day = 'Day02 of 30DaysChallenge'
+isComplete = False
+
+
+print('Value of pi: ', pi)
+print('Data type of pi: ', type(pi))
+
+
+# Non-Primitive
+fruits = ['apple', 'banana']
+nums = ('One', 1, 'Two', 2)
+companies = {'tesla', 'google', 'microsoft'}
+bill = {
+    "product": "Mouse",
+    "price": 10
+}
+
+
+print('Bill: ', bill)
+print('Data type of bill: ', type(bill))
