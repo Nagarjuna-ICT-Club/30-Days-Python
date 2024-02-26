6b39a6b94fdf03fe4d402c12c6080a3fe58ed2ad
NickName-AM <maharjanebik061@gmail.com>
2024-02-26 20:56:48 +0545

    day03

---
+++ Day03/day03.py
@@ -0,0 +1,13 @@
+a = [5, 2, 7, 1, 2, 3]
+
+# Add 6 at the end of the list
+a.append(6)
+print(a)
+
+# Sort the list in ascending order
+a.sort()
+print(a)
+
+# Remove an item at index 1
+a.pop(1)
+print(a)
