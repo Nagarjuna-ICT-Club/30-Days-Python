602bcd87355759200d98d9c30875d4a5c6a28f0b
NickName-AM <maharjanebik061@gmail.com>
2024-03-07 23:07:03 +0545

    day06

---
+++ Day06/day06.py
@@ -0,0 +1,16 @@
+a = 0
+
+while a < 10:
+    print(a)
+
+    # Increments the value of a by 1
+    a += 1
+
+c = 5
+
+while c > 0:
+    print(c)
+
+    # terminates the execution of while loop
+    break
+    c -= 1
